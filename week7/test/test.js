3ac3270d306b0bb20744dacad660ec021de42086
dannyb94 <danielle.ussery@bryanuniversity.edu>
2022-03-12 11:59:57 -0800

    week7

--- week7/test/test.js
+++ week7/test/test.js
@@ -2,7 +2,7 @@
 
 // Call the list   DONE
 // Add an item to the list   DONE
-// Add multiple items to the list   DONE
+// Add multiple items to the list   ERROR
 // Remove the first item from the list   DONE
 // Remove the last item from the list   DONE
 // Remove a specific item from the list   DONE
@@ -13,44 +13,47 @@ const chai = require('chai')
 const expect = chai.expect
 
 const Todo = require('../toDoClass')
-console.log(Todo)
 var newTodo = new Todo()
 
 it('add to list', () => {
 	expect(newTodo.add('finish capstone')).to.deep.equal(['finish capstone']) 
 })
 
-it('add to list', () => {
-	expect(newTodo.add(false)).to.deep.equal(['finish capstone', false]) 
+it('add multiple to list', () => {
+	expect(newTodo.addMul(false, 2064, {'store': 3})).to.deep.equal(['finish capstone', false, 2064, {'store': 3}]) 
 })
 
-it('add to list', () => {
-	expect(newTodo.add(2064)).to.deep.equal(['finish capstone', false, 2064]) 
-})
+// it('add to list', () => {
+// 	expect(newTodo.add(false)).to.deep.equal(['finish capstone', false]) 
+// })
 
-it('add to list', () => {
-	expect(newTodo.add({store: 3})).to.deep.equal(['finish capstone', false, 2064, {store: 3}]) 
-})
+// it('add to list', () => {
+// 	expect(newTodo.add(2064)).to.deep.equal(['finish capstone', false, 2064]) 
+// })
 
-it('call the list', () => {
-	expect(newTodo.list).to.deep.equal(['finish capstone', false, 2064, {store: 3}]) 
-})
+// it('add to list', () => {
+// 	expect(newTodo.add({store: 3})).to.deep.equal(['finish capstone', false, 2064, {store: 3}]) 
+// })
 
-it('search list and return true ', () => {
-	expect(newTodo.search(false)).to.deep.equal(true)		
-})
+// it('call the list', () => {
+// 	expect(newTodo.list).to.deep.equal(['finish capstone', false, 2064, {store: 3}]) 
+// })
 
-it('remove item from list ', () => {
-	expect(newTodo.remove('finish capstone')).to.deep.equal([false, 2064, {store: 3}]) 
-})
+// it('search list and return true ', () => {
+// 	expect(newTodo.search(false)).to.deep.equal(true)		
+// })
 
-it('remove first item from list ', () => {
-	expect(newTodo.removeFirst(false)).to.deep.equal([2064, {store: 3}]) 
-})
+// it('remove item from list ', () => {
+// 	expect(newTodo.remove('finish capstone')).to.deep.equal([false, 2064, {store: 3}]) 
+// })
 
-it('remove last item from list ', () => {
-	expect(newTodo.removeLast({store: 3})).to.deep.equal([2064]) 
-})
+// it('remove first item from list ', () => {
+// 	expect(newTodo.removeFirst(false)).to.deep.equal([2064, {store: 3}]) 
+// })
+
+// it('remove last item from list ', () => {
+// 	expect(newTodo.removeLast({store: 3})).to.deep.equal([2064]) 
+// })
 
 // beforeEach(function(){
 // 	newTodo = new Todo(['finish capstone', 'buy mushrooms', 'cook tofu'])
